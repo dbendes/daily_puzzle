5d7e4ff8bf55d0a805f083a1559b7b18f28e7f8d
David Bendes <david.bendes+github@gmail.com>
2015-08-08 23:57:36 +0000

    fixing the config web console whitelist

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module PuzzleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.web_console.whitelisted_ips = '184.153.75.239'
+    # config.web_console.whitelisted_ips = '184.153.75.239'
     
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
