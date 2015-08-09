24154265194eee53e3c7aee3c93e65a4ac84f93b
David Bendes <david.bendes+github@gmail.com>
2015-08-09 19:08:42 +0000

    formatting the score leaderboard

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,13 +3,20 @@ class Game < ActiveRecord::Base
     has_many :scores
     has_many :users, through: :scores
     
-    def top3
+    def top3_alltime
         self.scores.order(value: :desc).first(3)
     end
     
-    def top5
+    def top5_alltime
         self.scores.order(value: :desc).first(5)
     end
     
+    def top3_today
+        self.scores.where(date: Date.today).order(value: :desc).first(3)
+    end
+    
+    def top5_today
+        self.scores.where(date: Date.today).order(value: :desc).first(5)
+    end
     
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,38 @@ class Score < ActiveRecord::Base
   belongs_to :user
   validates :game, presence: true
   validates :user, presence: true
+  
+  def format_long
+    game = self.game_id
+    value = self.value
+    #if game has a time format, format as time
+    if game == 1
+      mins = (value/60.0).floor
+      secs = value%60
+      mins.floor.to_s + " " + "minute".pluralize(mins) + " " + (secs).to_s + " " +  "second".pluralize(secs)
+    # if game has a steps format, format as points
+    elsif game ==2
+      (value).to_s + " " + "step".pluralize(value)
+    # if a game has a points format, format as points
+    elsif game == 3
+      (value.to_s) + " " + "points".pluralize(value)
+    end
+  end
+  
+  def format_short
+    game = self.game_id
+    value = self.value
+    
+    #if game has a time format, format as time
+    if game == 1
+      (value / 60).floor.to_s + ":" + value%60.to_s
+    # if game has a steps format, format as points
+    elsif game ==2
+      value
+    # if a game has a points format, format as points
+    elsif game == 3
+      value
+    end
+  end
+  
 end
