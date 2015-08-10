3fe8f35b9e9fd25dd56318e896faa4a7994d6ace
David Bendes <david.bendes+github@gmail.com>
2015-08-10 03:39:02 +0000

    removing the thanks for playing

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,21 +2,30 @@ class Game < ActiveRecord::Base
     has_one :instruction
     has_many :scores
     has_many :users, through: :scores
-    
+
     def top3_alltime
         self.scores.order(value: :desc).first(3)
     end
-    
+
     def top5_alltime
         self.scores.order(value: :desc).first(5)
     end
-    
+
     def top3_today
         self.scores.where(date: Date.today).order(value: :desc).first(3)
     end
-    
+
     def top5_today
         self.scores.where(date: Date.today).order(value: :desc).first(5)
     end
-    
+
+    def scores_today
+        self.scores.where(date: Date.today)
+    end
+
+    def users_today
+        self.joins(:users, :scores).where(date: Date.today)
+    end
+
+
 end
