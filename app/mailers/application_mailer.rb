6468addd6a2095ea82f9e6c2fc67f60fff3caede
David Bendes <david.bendes+github@gmail.com>
2015-08-15 16:55:30 +0000

    set the from for an action mailer, fix the sizing on the all time scores tab

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < Devise::Mailer
-  default from: "info@dailypuzzl.es"
+  default from: '"Daily Puzzles" <info@dailypuzzl.es>'
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
