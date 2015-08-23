2f73c9c64d9d1c33fee908e345a63cda8de6e687
David Bendes <david.bendes+github@gmail.com>
2015-08-23 23:57:31 +0000

    groups can be private, checkbox form, private icon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816190532) do
+ActiveRecord::Schema.define(version: 20150823224355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20150816190532) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "private",    default: false
   end
 
   create_table "instructions", force: :cascade do |t|
