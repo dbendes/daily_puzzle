3fb54df89a71665ddf647a96661a461c63f56b9b
David Bendes <david.bendes+github@gmail.com>
2015-08-30 21:53:30 +0000

    mailer for admin access

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
         @user = user
         mail(to: user.email, subject: 'Welcome to DailyPuzzl.es!')
     end
+    def request_to_join_group_email(membership)
+        @group = Group.find(membership.group_id)
+        @user = User.find(membership.user_id)
+        @admin = Membership.find(role: 2, group_id: group.id).first.user
+        mail(to: @admin.email, subject: "Daily Puzzles: Request to join " + group.name)
+    end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,4 +3,14 @@ class Membership < ActiveRecord::Base
   belongs_to :user
 
   validates :group, uniqueness:  { scope: [:user] }
+
+  after_create :send_admin_email
+
+  def send_admin_email
+    if self.role == 1
+      UserMailer.request_to_join_group_email(self).deliver
+    end
+  end
+
+
 end
