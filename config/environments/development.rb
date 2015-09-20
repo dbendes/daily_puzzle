dc310d7bf5cfc1fe64eb29f5b837a3eb6d248c46
David Bendes <david.bendes+github@gmail.com>
2015-09-20 03:00:21 +0000

    cleaned up the group view, got the buttons on the same line

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 #MAILER CONFIG
-config.action_mailer.default_url_options = { host: $IP, port: $PORT }
+config.action_mailer.default_url_options = { :host => $IP, :port => $PORT }
 
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
