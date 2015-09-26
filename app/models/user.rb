9fb9ebb601cc4e55020d59d2e0f4af1d5e826c71
David Bendes <david.bendes+github@gmail.com>
2015-09-26 19:25:16 +0000

    changing the default avatar URL

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
     :styles => { :medium => "200x200>", :thumb => "100x100#" },
-    :default_url => "/images/:style/gravatar31.jpg",
+    :default_url => ":s3_domain_url/public/images/:style/gravatar31.jpg",
     :url  => ":s3_domain_url",
     :path => "public/avatars/:id/:style_:basename.:extension",
     :storage => :fog,
